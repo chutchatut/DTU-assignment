189daf28d37b29b0a2bf7dfffe0469779b84e0c8
Chutchatut Sutichavengkul <chutchatut@gmail.com>
2023-04-25 20:16:52 +0700

    add utils circular buffer

---
+++ kmer_counter/mine/utils/circular_buffer.h
@@ -0,0 +1,26 @@
+#include <string>
+
+template <size_t k>
+class CircularBuffer
+{
+private:
+    char data[k];
+    usize_t write_head;
+
+public:
+    Kmer() : write_head(0){};
+
+    void insert(char c)
+    {
+        data[write_head] = c;
+        write_head = (write_head + 1) % k;
+    }
+
+    std::string get_string()
+    {
+        std::string s;
+        for (int16_t i = 0; i < k; ++i)
+            s.push_back(data[(write_head + i) % k]);
+        return s;
+    }
+};
