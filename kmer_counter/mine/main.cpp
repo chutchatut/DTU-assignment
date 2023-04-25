1ae0762428040856fd624c86a665f532bf412678
Chutchatut Sutichavengkul <chutchatut@gmail.com>
2023-04-25 20:13:09 +0700

    update driver code variable name

--- kmer_counter/mine/main.cpp
+++ kmer_counter/mine/main.cpp
@@ -13,13 +13,13 @@ void test_insert(const char &c, const bool &is_ready)
 namespace fs = std::filesystem;
 int main(int argc, char *argv[])
 {
-    // input is k, fasta_path, write_path
+    // input is k, fasta_path, cache_path
     assert(argc == 4);
 
     // parse input
     uint16_t k = boost::lexical_cast<uint16_t>(argv[1]);
     fs::path fasta_path(argv[2]);
-    fs::path write_path(argv[3]);
+    fs::path cache_path(argv[3]);
 
     read_fasta(fasta_path, test_insert, k);
 }
