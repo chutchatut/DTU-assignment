1b1e66297593af0a7f1cbd2479960084fe65fe04
Chutchatut Sutichavengkul <chutchatut@gmail.com>
2023-04-25 20:10:02 +0700

    add guard condition for printing k-mer and add update comments

--- kmer_counter/mine/utils/cache_writer.h
+++ kmer_counter/mine/utils/cache_writer.h
@@ -11,15 +11,16 @@ class RadixCacheWriter
 {
 private:
     fs::path basepath;
+    std::string prev_prefix;
     bool spilled_to_disk;
     std::unordered_map<std::string, uint16_t> count_map;
     std::unordered_map<std::string, std::ofstream> prefix_to_writer;
 
-    std::ofstream get_writer(const std::string &prefix)
+    std::ofstream get_or_create_writer(const std::string &prefix)
     {
         if (prefix_to_writer.find(prefix) == prefix_to_writer.end())
         {
-            std::ofstream file_writer(basepath / prefix);
+            std::ofstream file_writer(basepath / (prev_prefix + prefix));
             prefix_to_writer[prefix] = file_writer;
         }
         return prefix_to_writer[prefix];
@@ -29,21 +30,28 @@ private:
     {
         spilled_to_disk = true;
         std::string prefix = s.substr(0, prefix_length);
-        std::ofstream file_writer = get_writer(prefix);
-        file_writer << s << ' ' << count << '\n';
+        std::string suffix = s.substr(prefix_length);
+        std::ofstream file_writer = get_or_create_writer(prefix);
+        file_writer << suffix << ' ' << count << '\n';
     }
 
 public:
-    RadixCacheWriter(fs::path basepath) : basepath(basepath), spilled_to_disk(false){};
+    RadixCacheWriter(fs::path basepath) : basepath(basepath), prev_prefix(prev_prefix), spilled_to_disk(false){};
 
     void insert(const std::string &s, uint16_t count)
     {
-        if ((count_map.size() < MAX_DICT_CAPACITY)    // dict is not full
-            || (count_map.find(s) != count_map.end()) // this string is already contained in dict
-            || (s.size() <= prefix_length))           // string size is shorter than prefix_length
-            ++count_map[s];
+        if ((count_map.size() < MAX_DICT_CAPACITY)    // Dict is not full
+            || (count_map.find(s) != count_map.end()) // This string is already contained in dict
+            || (s.size() <= prefix_length))           // String size is shorter than prefix_length
+        {
+            // Only increment if count is not max
+            if (count_map[s] != UINT16_MAX)
+                ++count_map[s];
+        }
         else
+        {
             write_to_disk(s, count);
+        }
     }
 
     ~RadixCacheWriter()
@@ -53,6 +61,7 @@ public:
                 write_to_disk(k, v);
         else // if not print to stdout
             for (const auto &[k, v] : count_map)
-                std::cout << k << ' ' << v << '\n';
+                if (v > 1)
+                    std::cout << prev_prefix << k << ' ' << v << '\n';
     }
 };
