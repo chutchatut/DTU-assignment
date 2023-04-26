e8d2096edff8333bd14acd049c44b0a79530d854
Chutchatut Sutichavengkul <chutchatut@gmail.com>
2023-04-26 11:47:18 +0700

    auto convert fasta to capital letter when reading

--- kmer_counter/mine/utils/fasta_reader.h
+++ kmer_counter/mine/utils/fasta_reader.h
@@ -5,7 +5,7 @@
 std::unordered_set<char> symbols({'A', 'T', 'C', 'G'});
 
 namespace fs = std::filesystem;
-void read_fasta(const fs::path &file_path, void (*insert_fn)(const char &, const bool &), uint16_t k)
+void read_fasta(const fs::path &file_path, const std::function<void(const char &, const bool &)> &insert_fn, uint16_t k)
 {
     bool is_reading_dna = false;
     char ch;
@@ -20,6 +20,7 @@ void read_fasta(const fs::path &file_path, void (*insert_fn)(const char &, const
         }
         if (ch == '\n')
             is_reading_dna = true;
+        ch = toupper(ch);
         if (is_reading_dna && (symbols.find(ch) != symbols.end()))
         {
             insert_fn(ch, i >= k - 1);
